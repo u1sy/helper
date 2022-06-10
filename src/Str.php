8e9b7667400eb873b4bc210ed328ad573d65a802
liu <liuwen890825>
2022-06-10 16:41:12 +0800

    检查字符串中是否包含某些字符串

--- src/Str.php
+++ src/Str.php
@@ -14,4 +14,21 @@ class Str
             . substr($chars, 20, 12);
         return $prefix . $uuid;
     }
+
+    /**
+     * 检查字符串中是否包含某些字符串
+     * @param string       $haystack
+     * @param string|array $needles
+     * @return bool
+     */
+    public static function contains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ('' != $needle && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
