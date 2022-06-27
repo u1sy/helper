d37520df556216993d3576190f8399bd4c4d8d7d
liu <liuwen890825>
2022-06-27 15:15:14 +0800

    ..

--- src/Arr.php
+++ src/Arr.php
@@ -18,4 +18,27 @@ class Arr
         }
         return array_diff_key($array, array_flip($keys));
     }
+
+
+    /**
+     * @param array $array
+     * @param int|null $amount
+     * @return array|mixed
+     */
+    public static function random(array $array, int $amount = null)
+    {
+        if (is_null($amount)) {
+            return $array[array_rand($array)];
+        }
+
+        $keys = array_rand($array, $amount);
+
+        $results = [];
+
+        foreach ((array) $keys as $key) {
+            $results[] = $array[$key];
+        }
+
+        return $results;
+    }
 }
